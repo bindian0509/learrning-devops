31e1d2985f682904e830f0d7591fecd441f419d0
Bharat Verma <bindian0509@gmail.com>
2023-08-09 15:58:54 +0530

    Trying out changes with respect to running war on docker based tomcat container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
 FROM tomcat:latest
 
-# copy war file on to container
-COPY ./iwayq.war /usr/local/tomcat/webapps
+# Remove the default Tomcat webapps
+RUN rm -rf /usr/local/tomcat/webapps/*
+
+# Copy your Spring application WAR file to the Tomcat webapps directory
+COPY ./iwayq.war /usr/local/tomcat/webapps/ROOT.war
+
+# Expose the Tomcat port
+EXPOSE 8080
+
+# Start the Tomcat server
+CMD ["catalina.sh", "run"]
---
+++ docker-commands.sh
@@ -0,0 +1,25 @@
+# create war file
+
+mvn -U deploy -s settings.xml
+
+# download war file
+
+curl --user "admin:RQb7ctKtx5KFP6q" http://localhost:8082/artifactory/libs-release/com/iwayq/iwayQApp/1.0-RELEASE/iwayQApp-1.0-RELEASE.war -o iwayq.war
+
+# build docker image
+
+docker build . --tag iwayq-tomcat
+
+# run docker image
+
+docker run --name iwayq-java-app -p 8080:8080 -d iwayq-tomcat
+
+
+# login into docker container
+docker exec -it iwayq-java-app bash
+
+# mysql docker container
+docker run --name mysql -p3306:3306 -e MYSQL_ROOT_PASSWORD=oracle -d mysql:latest
+
+# connect to mysql docker container
+mysql -h 127.0.0.1 -u root -p
