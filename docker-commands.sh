61c5be124989a49266d80bd25d1d09ce17fdc686
Bharat Verma <bharatverma@GPMAC-T3HGWFMHFW.local>
2023-08-14 23:02:48 +0530

    changes w.r.t. docker containers on new machine

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,7 +1,7 @@
 # to run a docker based artifactory container
 
 docker run --name artifactory -v /home/bharat/jfrog_artifactory_data:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:latest
-
+docker run --name artifactory -v /Users/bharatverma/docker-vols/jfrog_artifactory_data:/var/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-oss:latest
 # create war file
 
 mvn -U deploy -s settings.xml
@@ -20,7 +20,7 @@ docker run --name iwayq -p 8080:8080 -d iwayq
 
 
 # login into docker container
-docker exec -it iwayq-java-app bash
+docker exec -it iwayq bash
 
 # mysql docker container
 docker run --name mysql -p3306:3306 -e MYSQL_ROOT_PASSWORD=oracle -d mysql:latest
@@ -33,4 +33,4 @@ docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' iwa
 
 
 # sonarqube code check
- mvn clean verify sonar:sonar -Dsonar.projectKey=coding-with-java -Dsonar.projectName='coding-with-java' -Dsonar.host.url=http://localhost:9000 -Dsonar.token=sqp_3d15e66d0c50b3a74c52e6a1a5e455e18d124179
+mvn clean verify sonar:sonar -Dsonar.projectKey=coding-with-java -Dsonar.projectName='coding-with-java' -Dsonar.host.url=http://localhost:9000 -Dsonar.token=sqp_3d15e66d0c50b3a74c52e6a1a5e455e18d124179
